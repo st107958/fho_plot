fc5c08c1ae52dc255b4d4d2cf1d435ca72951ac4
st107958 <st107958@student.spbu.ru>
2024-10-27 21:28:20 +0300

    upd

--- fho_fr_counting.py
+++ fho_fr_counting.py
@@ -14,8 +14,8 @@ spectr_constants = {
 }
 pa_to_atm = 9.86923e-6
 
-molecule = 'NO-O' # тут менять
-particle = 'NO' # тут
+molecule = 'N2-O' # тут менять
+particle = 'N2' # тут
 
 lines_to_skip = 1
 def rel_times(temperature: np.array, coefficient: np.array, molecule: str):
